b8f51ab5126084d671a412968783de5b8bb6f8d1
sjm-L <shower1300@gmail.com>
2025-08-01 14:59:24 +0900

    asd

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const asset = {
-  first: "son",
-  second: "jm",
-};
-
-function third(object) {
-  if (typeof object === "object") {
-    let result = object.second + object.first;
-    return result;
-  } else {
-    // console.log("메개변수 객체여야됨요");
-    return "";
-  }
-}
-
-console.log(third(asset));
-console.log(third(1));
+console.log("안녕");
